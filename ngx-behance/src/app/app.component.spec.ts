cb9ac885885c7ce72b65d26eab64c7cb8a3f03ab
ChioSoCo <rociosolis092@gmail.com>
2019-11-04 18:19:37 -0600

    Update test case for title

--- ngx-behance/src/app/app.component.spec.ts
+++ ngx-behance/src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngx-behance'`, () => {
+  it(`should have as title 'DEMO Accenture'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('ngx-behance');
