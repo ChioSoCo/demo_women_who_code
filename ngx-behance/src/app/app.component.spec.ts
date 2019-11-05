64fda00b68000f96f5bb6a17fcb1c9e70426526c
ChioSoCo <rociosolis092@gmail.com>
2019-11-04 18:44:58 -0600

    Commenting async func on test file

--- ngx-behance/src/app/app.component.spec.ts
+++ ngx-behance/src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  /*
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -13,6 +14,7 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+  */
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
